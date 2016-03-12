7c5f0ce621dab508215a8a9b68ef79592c818dc9
Alex Ewerlöf <hanifbbz@gmail.com>
2016-03-12 10:12:17 +0100

    now we show the rows add/remove works load/save works too

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,58 @@
 import {Component} from 'angular2/core';
 
+const TABLE_STORAGE_KEY = '93488934-3433434-343943349438-XX';
+
+interface Row {
+    team1: string;
+    team2: string;
+    task: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>This is a basic Angular 2 App</h1>'
+    templateUrl: 'app.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    public rows:Row[] = [];
+    
+    constructor() {
+        try {
+            this.rows = this.load();
+        } catch (e) {
+            console.warn(`Could not load because ${e}`);
+        }
+    }
+    
+    public addRow() {
+        this.rows.push({team1: 'j', team2: 'd', task: 'f'});
+    }
+    
+    public removeRow(row:Row) {
+        var indexOfRow = this.rows.indexOf(row);
+        if (indexOfRow !== -1) {
+            this.rows.splice(indexOfRow, 1);
+        }
+    }
+    
+    private static isValidRow(row:Row) {
+        return typeof row.team1 === 'string'
+            && typeof row.team2 === 'string'
+            && typeof row.task === 'string';
+    }
+    
+    public load() {
+        var result = JSON.parse(localStorage.getItem(TABLE_STORAGE_KEY));
+        if (!Array.isArray(result)) {
+            throw 'The result format is not right';
+        }
+        if (result.every(AppComponent.isValidRow)) {
+            return result;
+        } else {
+            throw 'The result was not an array';
+        }
+    }
+    
+    public save() {
+        localStorage.setItem(TABLE_STORAGE_KEY, JSON.stringify(this.rows));
+    }
+}
